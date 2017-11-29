a1bf746d48d0f81e9c9e52fa0412bda7f2b4670c
Caroline Gard <carga635@student.liu.se>
2017-11-29 16:17:33 +0100

    Added sphere for background in addLight.py

--- Scripts/addLight.py
+++ Scripts/addLight.py
@@ -2,6 +2,10 @@
 
 import maya.cmds as cmds
 
+# Create the groundPlane
+cmds.polySphere(sx=100, sy=100, r=50, n='backgroundSphere')
+cmds.setAttr( 'lambert1.color', 0.74, 0.99, 0.79, type = 'double3' ) 
+
 # Create ambient light
 light = cmds.ambientLight(intensity=0.8)
 cmds.ambientLight( light, e=True, ss=True, intensity=0.5, n='lightAmb')
@@ -14,4 +18,3 @@ cmds.move( 0.0, 5.0, 0.0 )
 # Query it
 cmds.ambientLight( light, q=True, intensity=True )
 cmds.directionalLight( light, q=True, intensity=True )
-
