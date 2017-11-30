d9ad9c82c8377e7280680f67272aa7210070a957
Kristin Bäck <kriba265@student.liu.se>
2017-11-30 12:01:43 +0100

    added gravity and changed color of particles

--- Scripts/createTransparentBox.py
+++ Scripts/createTransparentBox.py
@@ -5,6 +5,13 @@ import maya.cmds as cmds
 # Run mel file for creating particles
 # Solve: Read mel file from python or create particle fill 
 
+
+#Set the playback options
+cmds.playbackOptions(playbackSpeed = 0, maxPlaybackSpeed = 1)
+cmds.playbackOptions(min = 1, max = 300)
+cmds.currentTime(1)
+
+
 # Delete Cube created in mel file
 cmds.select( 'cubeFill' )
 cmds.delete()
@@ -30,3 +37,25 @@ cmds.setAttr( 'lambert1.transparency', 0.922581, 0.922581, 0.922581, type = 'dou
 cmds.setAttr( 'lambert1.refractions', 1 )
 cmds.setAttr( 'lambert1.refractiveIndex', 1.52 )
 cmds.setAttr( 'lambert1.color', 0.417, 0.775769, 1, type = 'double3' )
+
+
+#Remove maya-initialised values
+cmds.select( 'nParticle1' )
+cmds.setAttr( 'nParticleShape1.ignoreSolverGravity', 1)
+cmds.setAttr( 'nParticleShape1.particleRenderType', 4)     #sets particle to being spheres
+cmds.setAttr( 'nParticleShape1.bounce', 0)
+cmds.setAttr( 'nParticleShape1.friction', 0)
+
+#Color the particles
+cmds.setAttr( 'nParticleShape1.color[0].color_Color', 0.3, 0.657, 0.657, type = 'double3' )
+cmds.setAttr( 'nParticleShape1.color[1].color_Color', 0.3, 0.657, 0.657, type = 'double3' )
+
+#create gravity and attatch to particles
+cmds.gravity( 'nParticle1', n= 'gravity' ) 
+cmds.setAttr( 'gravity.attenuation', 1)
+cmds.connectDynamic( 'nParticle1', f ='gravity')
+
+'''
+cmds.rigidBody( n='myRigidBody', active=True, iv=(10, 0, 0), b=0.5, sf=0.4 )
+cmds.connectDynamic( 'nParticle1', f ='myRigidBody')
+'''
